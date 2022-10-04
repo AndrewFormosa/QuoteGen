71e6a0f707a3d74d01e0e56048ff83066b16803b
Andrew Formosa <andyformosa@aol.com>
2022-10-04 19:27:27 +0100

    1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>This will be Andrew Formosa's 1first React App</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Andrew Formosa</h1>
     </div>
   );
 }
+
+
 
 export default App;
