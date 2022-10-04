600f4f2db5977d112909354963d889123d84c258
Andrew Formosa <andyformosa@aol.com>
2022-10-04 16:30:17 +0100

    1

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>This will be Andrew Formosa's first React App</p>
         <a
           className="App-link"
           href="https://reactjs.org"
