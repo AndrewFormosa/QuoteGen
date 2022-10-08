8fc03ff7ac98b0cfb175387062e02a9e9b270ad5
Andrew Formosa <andyformosa@aol.com>
2022-10-08 15:59:46 +0100

    1

--- src/MyApp.js
+++ src/MyApp.js
@@ -4,14 +4,16 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery'
 import quotes from './quotes.js';
+import variables from './MyApp.scss';
 
-let bgColor = "rgb(85, 101, 174)";
 
 let randomeQuoteNumber = () => {
   return (Math.floor(Math.random() * Object.keys(quotes).length))
 }
 
 let firstQuoteNumber = randomeQuoteNumber();
+let bg=variables.bgColor;
+
 
 
 class QuoteDisplayer extends React.Component {
@@ -20,6 +22,10 @@ class QuoteDisplayer extends React.Component {
     this.state = { AllQuotes: quotes, QuoteNumber: firstQuoteNumber }
     this.handleClick = this.handleClick.bind(this);
   }
+ 
+  componentDidMount(){
+    document.body.style.backgroundColor = bg;
+  }
 
   handleClick() {
     let newNumber = randomeQuoteNumber();
@@ -27,14 +33,15 @@ class QuoteDisplayer extends React.Component {
     $(".fade-out").animate({ opacity: '1.0' }, "slow");
   }
 
-
   render() {
+   
     let quotetext=this.state.AllQuotes[this.state.QuoteNumber].quote;
-    let twitterHref="https://twitter.com/intent/tweet?text="+quotetext;
+    let quoteauthor=this.state.AllQuotes[this.state.QuoteNumber].author;
+    let twitterHref="https://twitter.com/intent/tweet?text="+quotetext+" -"+quoteauthor;
     return (
-      <div id="quote-box" class="card shadow">
-        <figure class="text-end m-5 ">
-          <div class="fade-out">
+      <div id="quote-box" class="card shadow fade-out">
+        <figure class="text-end">
+          <div>
             <blockquote class="blockquote">
               <p id="text"><q>{quotetext}</q></p>
             </blockquote>
@@ -46,7 +53,7 @@ class QuoteDisplayer extends React.Component {
       
 
           </div>
-        <a class="twitter-share-button" id="tweet-quote" href={twitterHref} data-size="large">Share ths quote..<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16">
+        <a class="twitter-share-button d-flex justify-content-center" id="tweet-quote" href={twitterHref} data-size="large">Share ths quote..<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16">
   <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/>
 </svg></a>
         </figure>
@@ -55,25 +62,48 @@ class QuoteDisplayer extends React.Component {
   }
 }
 
+class RanGenHeader extends React.Component{
+render(){
+  return(
+    <div class="container-fluid">
+    <h1><svg xmlns="http://www.w3.org/2000/svg" width='6vw' height="6vw" fill="currentColor" class="bi bi-emoji-smile" viewBox="0 0 16 16">
+  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+  <path d="M4.285 9.567a.5.5 0 0 1 .683.183A3.498 3.498 0 0 0 8 11.5a3.498 3.498 0 0 0 3.032-1.75.5.5 0 1 1 .866.5A4.498 4.498 0 0 1 8 12.5a4.498 4.498 0 0 1-3.898-2.25.5.5 0 0 1 .183-.683zM7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5zm4 0c0 .828-.448 1.5-1 1.5s-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5z"/>
+</svg> Happy Thoughts...</h1>
+    </div>
 
+  );
+}
+
+}
 
 
 class MyApp extends React.Component {
   constructor(props) {
     super(props);
     this.state = { quotes }
+    
   }
-  render() {
+changeBackground(){return
+    $(document.body.style.background=variables.bgColor);
+  }
+
+  render() 
+  {
 
     return (
+      
       <div class="container-fluid my-5" >
+        <RanGenHeader/>
         <div class="animate">
           <QuoteDisplayer />
         </div>
+        <div class="text-center footer">Random Quote Generator Project by Andrew Formosa</div>
       </div>
     );
   }
 }
 
-export { MyApp };
+export { MyApp};
+
 
--- src/quotes.js
+++ src/quotes.js
@@ -1,39 +1,39 @@
 const quotes = {
     0: {
-      quote:"on day is great",
-      author:"me"
+      quote:"Wellness is not a ‘medical fix’ but a way of living – a lifestyle sensitive and responsive to all the dimensions of body, mind, and spirit, an approach to life we each design to achieve our highest potential for well-being now and forever.",
+      author:"Greg Anderson"
     },
     1: {
-      quote:"hello everyone",
-      author:"adam"
+      quote:"Wellness encompasses a healthy body, a sound mind, and a tranquil spirit. Enjoy the journey as you strive for wellness.",
+      author:"Laurette Gagnon Beaulieu"
     },
     2: {
-      quote:"yo dudes",
-      author:"finn"
+      quote:"Our bodies are our gardens – our wills are our gardeners.",
+      author:"William Shakespeare"
     },
     3: {
-      quote:"",
-      author:""
+      quote:"I have chosen to be happy because it is good for my health.",
+      author:"Voltaire"
     },
     4: {
-      quote:"",
-      author:""
+      quote:"Self-care is not selfish. You cannot serve from an empty vessel.",
+      author:"Eleanor Brown"
     },
     5: {
-      quote:"",
-      author:""
+      quote:"Today is your day to start fresh, to eat right, to train hard, to live healthy, to be proud.",
+      author:"Bonnie Pfiester"
     },
     6: {
-      quote:"",
-      author:""
+      quote:"A healthy attitude is contagious but don’t wait to catch it from others. Be a carrier.",
+      author:"Tom Stoppard"
     },
     7: {
-      quote:"",
-      author:""
+      quote:"If you keep good food in your fridge, you will eat good food.",
+      author:"Errick McAdams"
     },
     8: {
-      quote:"",
-      author:""
+      quote:"Keeping your body healthy is an expression of gratitude to the whole cosmos- the trees, the clouds, everything.",
+      author:"Thich Nhat Hanh"
     }
   };
 
